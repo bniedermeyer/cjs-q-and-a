7fdf1d28ff428d3cb0231250183abc9a2d4810a2
Brenden Niedermeyer <contact@brenden.dev>
2020-08-24 23:55:02 -0700

    wrap ask logic in trycatch

--- src/http/post-ask/index.js
+++ src/http/post-ask/index.js
@@ -9,27 +9,30 @@ exports.handler = arc.http.async(ask);
  */
 async function ask(req) {
   const { question, key } = req.body;
+  try {
+    if (question) {
+      //only keep each question for 30 minutes. This will help prevent having to clear questions
+      const ttl = Date.now() / 1000 + 1800;
 
-  if (question) {
-    //only keep each question for 30 minutes. This will help prevent having to clear questions
-    const ttl = Date.now() / 1000 + 1800;
+      await data.set({
+        table: "questions",
+        timesAsked: 1,
+        question,
+        ttl,
+      });
+      return { statusCode: 201 };
+    }
 
-    await data.set({
-      table: "questions",
-      timesAsked: 1,
-      question,
-      ttl,
-    });
-    return { statusCode: 201 };
-  }
-
-  if (key) {
-    await data.incr({
-      table: "questions",
-      prop: "timesAsked",
-      key,
-    });
-    return { statusCode: 200 };
+    if (key) {
+      await data.incr({
+        table: "questions",
+        prop: "timesAsked",
+        key,
+      });
+      return { statusCode: 200 };
+    }
+  } catch (error) {
+    return { statusCode: 500, message: error.message };
   }
 
   // either question or key are required
