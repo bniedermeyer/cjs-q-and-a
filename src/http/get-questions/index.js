e233246b6e295394eeff555dfbc4b19f5ac82174
Brenden Niedermeyer <contact@brenden.dev>
2020-08-25 20:14:28 -0700

    use paging to fetch questions

--- src/http/get-questions/index.js
+++ src/http/get-questions/index.js
@@ -10,7 +10,16 @@ exports.handler = arc.http.async(questions);
  * returns a list sorted by times the question has been asked descending
  */
 async function questions() {
-  const questions = await data.get({ table: "questions" });
+  const table = "questions";
+  let questions = await data.get({ table });
+
+  if (questions.cursor) {
+    // questions remaining data from begin
+    const cursor = questions.cursor;
+    const nextPage = await data.get({ table, cursor });
+    questions = questions.concat(nextPage);
+  }
+  console.log("questions retrieved: ", questions);
   const timeNow = new Date();
   const sortedQuestions = questions
     // do not return expired questions
